40306c3841c6d8f3c18ffe01d4c633b1afded697
lucaxgomex <lucas.g.oliv.dev@gmail.com>
2021-05-30 16:51:18 -0300

    Created the project homepage

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'home/index.html', {})
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
